f52714304153ee17e5863a7a3e47095602b5de95
tikskit <tikskit@gmail.com>
2024-01-30 08:41:41 +0700

    добавлен микросервис

---
+++ 08 Принципы организации микросервисов. Типы взаимодействий/main/src/main/java/ru/luzhnykh/socialnet/controller/DialogController.java
@@ -0,0 +1,22 @@
+package ru.luzhnykh.socialnet.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import ru.luzhnykh.socialnet.service.TokenService;
+
+@RestController
+@RequiredArgsConstructor
+public class DialogController {
+    private final TokenService tokenService;
+
+    @PostMapping("/dialog/{user_id}/send")
+    public ResponseEntity<String> send(@PathVariable String user_id, @RequestBody String text, @RequestHeader String token) {
+        if (tokenService.validate(token)) {
+//            return ResponseEntity.ok(userService.search(firstName, lastName));
+        } else {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+        }
+    }
+}
