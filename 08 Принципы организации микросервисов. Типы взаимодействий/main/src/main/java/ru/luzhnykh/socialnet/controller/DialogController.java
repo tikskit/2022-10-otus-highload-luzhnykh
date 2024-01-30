f3956c478f4e61ab9323405a3fbc9daefbc473fd
tikskit <tikskit@gmail.com>
2024-01-30 13:20:44 +0700

    redirect

--- 08 Принципы организации микросервисов. Типы взаимодействий/main/src/main/java/ru/luzhnykh/socialnet/controller/DialogController.java
+++ 08 Принципы организации микросервисов. Типы взаимодействий/main/src/main/java/ru/luzhnykh/socialnet/controller/DialogController.java
@@ -3,21 +3,18 @@ package ru.luzhnykh.socialnet.controller;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-import ru.luzhnykh.socialnet.service.TokenService;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.net.URI;
+import java.net.URISyntaxException;
 
 @RestController
 @RequiredArgsConstructor
 public class DialogController {
-    private final TokenService tokenService;
-
     @PostMapping("/dialog/{user_id}/send")
-    public ResponseEntity<String> send(@PathVariable String user_id, @RequestBody String text, @RequestHeader String token) {
-        if (tokenService.validate(token)) {
-//            return ResponseEntity.ok(userService.search(firstName, lastName));
-            return ResponseEntity.ok("ok");
-        } else {
-            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
-        }
+    ResponseEntity send(@PathVariable String user_id) throws URISyntaxException {
+        return ResponseEntity.status(HttpStatus.PERMANENT_REDIRECT).location(new URI(String.format("http://localhost:8081/dialog/%s/send", user_id))).build();
     }
 }
