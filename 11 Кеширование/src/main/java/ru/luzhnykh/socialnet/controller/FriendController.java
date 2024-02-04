41d5176c7ce10351ba502fde8b02ce65d9718692
tikskit <tikskit@gmail.com>
2024-02-04 21:22:01 +0700

    FriendService

--- 11 Кеширование/src/main/java/ru/luzhnykh/socialnet/controller/FriendController.java
+++ 11 Кеширование/src/main/java/ru/luzhnykh/socialnet/controller/FriendController.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import ru.luzhnykh.socialnet.service.FriendService;
 import ru.luzhnykh.socialnet.service.TokenService;
 
 /**
@@ -13,19 +14,21 @@ import ru.luzhnykh.socialnet.service.TokenService;
 @RequiredArgsConstructor
 public class FriendController {
     private final TokenService tokenService;
+    private final FriendService friendService;
 
     /**
      * Добавить в друзья
      *
      * @param actorUserId ИД пользователя, совершающего действие
-     * @param user_id     ИД пользователя, которого добавляют в друзья
+     * @param userId     ИД пользователя, которого добавляют в друзья
      * @param token       Токен
      */
     @PutMapping("/friend/set/{user_id}")
-    public ResponseEntity<String> set(@PathVariable("user_id") String actorUserId, @RequestParam String user_id,
+    public ResponseEntity<String> set(@PathVariable("user_id") String actorUserId, @RequestParam("user_id") String userId,
                                       @RequestHeader String token) {
         if (tokenService.validate(token)) {
-            return null;
+            friendService.addFriend(actorUserId, userId);
+            return ResponseEntity.ok("Пользователь успешно указал своего друга");
         } else {
             return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
         }
@@ -35,14 +38,15 @@ public class FriendController {
      * Отфрендить
      *
      * @param actorUserId ИД пользователя, совершающего действие
-     * @param user_id     Френд пользователя
+     * @param userId     Френд пользователя
      * @param token       Токен
      */
     @PutMapping("/friend/delete/{user_id}")
-    public ResponseEntity<String> delete(@PathVariable("user_id") String actorUserId, @RequestParam String user_id,
+    public ResponseEntity<String> delete(@PathVariable("user_id") String actorUserId, @RequestParam("user_id") String userId,
                                          @RequestHeader String token) {
         if (tokenService.validate(token)) {
-            return null;
+            friendService.delFriend(actorUserId, userId);
+            return ResponseEntity.ok("Пользователь успешно удалил из друзей пользователя");
         } else {
             return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
         }
