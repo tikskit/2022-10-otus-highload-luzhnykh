aea3a02b9c76e8120e89385a544f0787771a40fd
tikskit <tikskit@gmail.com>
2024-02-04 21:36:43 +0700

    FriendService

--- 11 Кеширование/postgre.cmd
+++ 11 Кеширование/postgre.cmd
@@ -1,1 +1,1 @@
-docker run -d -p 5432:5432 -e POSTGRES_PASSWORD=pass -e POSTGRES_USER=postgres -e POSTGRES_DB=soc_net postgres:16
+docker run -d -p 5432:5432 -v "F:\OTUS\HighLoad\db\hw11\socnet:/var/lib/postgresql/data" -e POSTGRES_PASSWORD=pass -e POSTGRES_USER=postgres -e POSTGRES_DB=soc_net postgres:16
