d4c21922b4f0dbd8ae5ddb9ef22aa44e79cda347
tikskit <tikskit@gmail.com>
2024-04-05 11:15:33 +0700

    Initial commit

---
+++ 27 Распределенные транзакции/documents/src/main/java/ru/luzhnykh/distribtx/documents/DocumentsApplication.java
@@ -0,0 +1,13 @@
+package ru.luzhnykh.distribtx.documents;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocumentsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DocumentsApplication.class, args);
+	}
+
+}
---
+++ 27 Распределенные транзакции/documents/src/test/java/ru/luzhnykh/distribtx/documents/DocumentsApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.luzhnykh.distribtx.documents;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocumentsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
